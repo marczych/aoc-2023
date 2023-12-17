529156730de635d440157af6ab1d14435e977709
Marc Zych <marczych@gmail.com>
2023-12-16 22:30:14 -0800

    Day 2 Part 2

--- day2/solution.go
+++ day2/solution.go
@@ -10,78 +10,48 @@ import (
 	"strings"
 )
 
-var colorCounts = map[string]int{
-	"red":   12,
-	"green": 13,
-	"blue":  14,
-}
-
-func getValidGameValue(gameLine string) int {
+func getGamePower(gameLine string) int {
 	splitLine := strings.Split(gameLine, ": ")
 	if len(splitLine) != 2 {
 		log.Fatal(fmt.Sprintf("Invalid game: %s", gameLine))
 	}
 
-	gameNumber := getGameNumber(splitLine[0])
-
-	if isValidGame(splitLine[1]) {
-		return gameNumber
+	handfuls := strings.Split(splitLine[1], "; ")
+	colorCounts := map[string]int{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
 	}
 
-	return 0
-}
-
-func getGameNumber(gameString string) int {
-	splitGameString := strings.Split(gameString, " ")
-	if len(splitGameString) != 2 {
-		log.Fatal(fmt.Sprintf("Invalid game prefix: %s", gameString))
-	}
-
-	gameNumber, err := strconv.Atoi(splitGameString[1])
-	if err != nil {
-		log.Fatal(fmt.Sprintf("Invalid game number: %s", err))
-	}
-
-	return gameNumber
-}
-
-func isValidGame(game string) bool {
-	handfuls := strings.Split(game, "; ")
 	for _, handful := range handfuls {
-		if !isValidHandful(handful) {
-			return false
+		cubes := strings.Split(handful, ", ")
+		for _, cubeString := range cubes {
+			cubeParts := strings.Split(cubeString, " ")
+			if len(cubeParts) != 2 {
+				log.Fatal(fmt.Sprintf("Invalid cubes: %s", handful))
+			}
+
+			count, err := strconv.Atoi(cubeParts[0])
+			if err != nil {
+				log.Fatal(fmt.Sprintf("Invalid count: %s", err))
+			}
+
+			color := cubeParts[1]
+			maxCubeCount, ok := colorCounts[color]
+
+			if !ok {
+				log.Fatal(fmt.Sprintf("Invalid color: %s", color))
+			}
+
+			colorCounts[color] = max(maxCubeCount, count)
 		}
 	}
 
-	return true
-}
-
-func isValidHandful(handful string) bool {
-	cubes := strings.Split(handful, ", ")
-	for _, cubeString := range cubes {
-		cubeParts := strings.Split(cubeString, " ")
-		if len(cubeParts) != 2 {
-			log.Fatal(fmt.Sprintf("Invalid cubes: %s", handful))
-		}
-
-		count, err := strconv.Atoi(cubeParts[0])
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Invalid count: %s", err))
-		}
-
-		color := cubeParts[1]
-		maxCubeCount, ok := colorCounts[color]
-
-		if !ok {
-			log.Fatal(fmt.Sprintf("Invalid color: %s", color))
-		}
-
-		if count > maxCubeCount {
-			return false
-		}
+	power := 1
+	for _, count := range colorCounts {
+		power *= count
 	}
-
-	return true
+	return power
 }
 
 func main() {
@@ -98,7 +68,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		total += getValidGameValue(line)
+		total += getGamePower(line)
 	}
 
 	fmt.Println(total)
