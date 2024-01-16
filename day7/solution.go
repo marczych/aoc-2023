c42b81cc7c764e0fc4e0cd42206320144b90dbbc
Marc Zych <marczych@gmail.com>
2024-01-15 18:59:37 -0800

    Day 7 Part 2

--- day7/solution.go
+++ day7/solution.go
@@ -33,16 +33,17 @@ func parseHand(line string) Hand {
 
 func calculateHandValue(hand string) int {
 	cardValues := map[rune]int{
-		'2': 0,
-		'3': 1,
-		'4': 2,
-		'5': 3,
-		'6': 4,
-		'7': 5,
-		'8': 6,
-		'9': 7,
-		'T': 8,
-		'J': 9,
+		// Now a Joker which is least valuable on its own.
+		'J': 0,
+		'2': 1,
+		'3': 2,
+		'4': 3,
+		'5': 4,
+		'6': 5,
+		'7': 6,
+		'8': 7,
+		'9': 8,
+		'T': 9,
 		'Q': 10,
 		'K': 11,
 		'A': 12,
@@ -62,11 +63,33 @@ func calculateHandType(hand string) int {
 		cardsMap[card] += 1
 	}
 
+	// Count the jokers and remove them from the map because we're going to handle them specially.
+	jokerCount := cardsMap['J']
+	delete(cardsMap, 'J')
 	cardCounts := make([]int, 0, 5)
 	for _, count := range cardsMap {
 		// Filter out single cards because they aren't useful.
 		if count > 1 {
 			cardCounts = append(cardCounts, count)
+		}
+	}
+
+	sort.Slice(cardCounts, func(i, j int) bool {
+		return cardCounts[i] > cardCounts[j]
+	})
+
+	// Special case for all jokers.
+	if jokerCount == 5 {
+		return FIVE_KIND_RANK
+	}
+
+	if jokerCount > 0 {
+		// If we have no duplicate cards but we have jokers, we can make some duplicates.
+		if len(cardCounts) == 0 {
+			cardCounts = append(cardCounts, jokerCount+1)
+		} else {
+			// Otherwise, each joker adds another of the card we have the most.
+			cardCounts[0] += jokerCount
 		}
 	}
 
@@ -84,7 +107,7 @@ func calculateHandType(hand string) int {
 			return TWO_KIND_RANK
 		}
 	} else if len(cardCounts) == 2 {
-		if cardCounts[0] == 3 || cardCounts[1] == 3 {
+		if cardCounts[0] == 3 {
 			return FULL_HOUSE_RANK
 		} else {
 			return TWO_PAIR_RANK
