43037a398fa99e0e3f082998d51b4303b7970830
Marc Zych <marczych@gmail.com>
2024-01-28 20:36:43 -0800

    Day 12 Part 2

--- day12/solution.go
+++ day12/solution.go
@@ -13,9 +13,23 @@ import (
 var OPERATIONAL = '.'
 var DAMAGED = '#'
 
-func getPossibilities(pattern string, groups []int) int {
+func getPossibilities(pattern string, groups []int, cache map[int]int) int {
 	if len(groups) == 0 {
 		return 0
+	}
+
+	// The cache is unique to this puzzle so we can crudely create an integer
+	// cache key by taking the lengths of the inputs and combining their values
+	// in a non-clashing way.
+	cacheKey := len(pattern)*1_000_000 + len(groups)
+
+	if result, ok := cache[cacheKey]; ok {
+		return result
+	}
+
+	returnCached := func(result int) int {
+		cache[cacheKey] = result
+		return result
 	}
 
 	requiredSize := len(groups) - 1
@@ -24,7 +38,7 @@ func getPossibilities(pattern string, groups []int) int {
 	}
 
 	if len(pattern) < requiredSize {
-		return 0
+		return returnCached(0)
 	}
 
 	valid := true
@@ -59,17 +73,17 @@ func getPossibilities(pattern string, groups []int) int {
 		} else {
 			// This is a valid position for this group so now we have to check
 			// the rest.
-			possibilities += getPossibilities(pattern[groups[0]+1:], groups[1:])
+			possibilities += getPossibilities(pattern[groups[0]+1:], groups[1:], cache)
 		}
 	}
 
 	// Only check possibilities for the current group moved over by one if the
 	// first position isn't definitely damaged.
 	if pattern[0] != byte(DAMAGED) {
-		possibilities += getPossibilities(pattern[1:], groups)
+		possibilities += getPossibilities(pattern[1:], groups, cache)
 	}
 
-	return possibilities
+	return returnCached(possibilities)
 }
 
 func main() {
@@ -87,17 +101,27 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		splitLine := strings.Split(line, " ")
-		pattern := splitLine[0]
-		countStrings := strings.Split(splitLine[1], ",")
-		counts := make([]int, 0, len(countStrings))
-		for _, countString := range countStrings {
+		originalPattern := splitLine[0]
+		originalCounts := splitLine[1]
+
+		pattern := originalPattern
+		countStrings := originalCounts
+		for i := 0; i < 4; i += 1 {
+			pattern += "?" + originalPattern
+			countStrings += "," + originalCounts
+		}
+
+		splitCounts := strings.Split(countStrings, ",")
+		counts := make([]int, 0, len(splitCounts))
+
+		for _, countString := range splitCounts {
 			count, err := strconv.Atoi(countString)
 			if err != nil {
 				log.Fatal("Failed to parse int: ", countString)
 			}
 			counts = append(counts, count)
 		}
-		totalArrangements += getPossibilities(pattern, counts)
+		totalArrangements += getPossibilities(pattern, counts, make(map[int]int))
 	}
 
 	fmt.Println(totalArrangements)
